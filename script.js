536dc004a676873d03bf6f52f43f6e64c1ab03fc
justingh1 <48250536+justingh1@users.noreply.github.com>
2019-03-05 09:24:43 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,15 @@
+$("#mole1").click(function(){
+    $("#mole1").hide();
+});
+    
+$("#moleTwo").click(function(){
+    $("#moleTwo").hide();
+}); 
+    
+$("#thirdMole").click(function(){
+    $("#thirdMole").hide();
+});
+    
+$("button").click(function(){
+    $("img").show();
+});
